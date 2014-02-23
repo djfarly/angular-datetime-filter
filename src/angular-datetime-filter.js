f456853bd674bb650b104ee5ddd51c626d779a16
djfarly <jan@mpfilm.de>
2014-02-23 13:40:02 +0100

    added basic files

---
+++ src/angular-datetime-filter.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+* datetimeFilter Module
+*
+* Description
+*/
+angular.module('datetimeFilter', [])
+	.filter('upcoming', [function() {
+		return function(array, dateField) {
+			if (!Array.isArray(array)) return array;
+			var now = new Date();
+			var filtered = [];
+		    for (var j = 0; j < array.length; j++) {
+		      	var value = array[j];
+		      	var then = new Date(value[dateField]);
+		      	if (now.getTime() < then.getTime()) {
+		        	filtered.push(value);
+		      	}
+		    }
+		    return filtered;
+		}
+	}])

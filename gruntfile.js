97de23628d1878bc244e92cc6e9b92ad604d7a7c
djfarly <jan@mpfilm.de>
2014-02-23 22:05:16 +0100

    missing ,

--- gruntfile.js
+++ gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function(grunt) {
       options: {
         configFile: 'test/karma.conf.js'
       }
-    }
+    },
     bump: {
       options: {
         files: ['package.json'],
